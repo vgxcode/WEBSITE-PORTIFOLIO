f623b829cae2b6dbebeb0ed492d4e8a40a351f6c
matheus <matheuzbonamente@gmail.com>
2024-02-20 00:37:44 -0300

    agr vai

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-            Hello, I&quot;m{" "}
+            Hello, I&apos;m{" "}
             </span>
             <br></br>
             <TypeAnimation
