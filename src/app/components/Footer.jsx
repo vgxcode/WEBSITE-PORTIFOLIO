23b65a2192755d192efb18af8982f5a2e1159bbe
matheus <matheuzbonamente@gmail.com>
2024-02-20 00:30:48 -0300

    fixed files 2

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
+      <div className="container p-11 flex justify-between">
         <span>mb</span>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
